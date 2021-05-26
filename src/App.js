43ff286dc0bb148cd164fa7135f6be0722365842
Philipp Thuerwaechter <pithu@thuerwaechter.de>
2021-05-26 21:48:39 +0200

    Basic clock version

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from "react";
+import { DateTimeFormatter, LocalDateTime } from "@js-joda/core";
 import './App.css';
 
+const dateFormatter = DateTimeFormatter.ofPattern("dd MM uuuu");
+const timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+
 function App() {
+  const [ldt, setLdt] = useState(LocalDateTime.now());
+
+  useEffect(() => {
+      const intervall = setInterval(() => setLdt(LocalDateTime.now()))
+      return () => {
+          clearInterval(intervall);
+      }
+  })
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <div className="clock">{ldt.format(dateFormatter)}</div>
+          <div className="clock">{ldt.format(timeFormatter)}</div>
       </header>
     </div>
   );
